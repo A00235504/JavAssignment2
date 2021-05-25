98a6e4b04e43f761232233665bc7391e711ebaab
aakashsadnani <A00235504@mycambrian.ca>
2021-05-26 01:56:39 +0530

    average and arraycontains done user i/p pending

--- Assignment2.java
+++ Assignment2.java
@@ -18,26 +18,55 @@ public class Assignment2 {
     public static void main(String[] args) {
         Scanner inputScannerValue = new Scanner(System.in);
 
-        
-        int[] myOrignalArray = {0,1,2,3,4,5,6,7,8,9};
-        
+        System.out.println("Enter search value");
+        int searchnumber = inputScannerValue.nextInt();
+
+        int[] myOrignalArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
         int[] myReverseArray = reverseArray(myOrignalArray);
 
-        for(int i=0;i<myReverseArray.length;i++){
+        for (int i = 0; i < myReverseArray.length; i++) {
             System.out.println(myReverseArray[i]);
 
         }
+        int averageValue = averageArray(myOrignalArray);
 
+        System.out.println("The average value of Array is: " + averageValue);
+
+        boolean search = arrayContains(myOrignalArray, searchnumber);
+
+        System.out.println("The search is: " + search);
     }
 
     public static int[] reverseArray(int[] inputarray) {
-    int reversingArray[] = new int[inputarray.length];
+        int reversingArray[] = new int[inputarray.length];
         int j = inputarray.length;
-        for(int i=0; i<inputarray.length; i++){
-            reversingArray[j-1] = inputarray[i];
-            j=j-1;
+        for (int i = 0; i < inputarray.length; i++) {
+            reversingArray[j - 1] = inputarray[i];
+            j = j - 1;
         }
         return reversingArray;
+    }
+
+    public static int averageArray(int[] averagearray) {
+        int result = 0;
+        for (int i = 0; i < averagearray.length; i++) {
+            result = result + averagearray[i];
+        }
+        return result;
+    }
+
+    public static boolean arrayContains(int[] arrayContaining, int searchnu) {
+        boolean mybool = false;
+        for (int i = 0; i < arrayContaining.length; i++) {
+            if (searchnu == arrayContaining[i]) {
+                mybool = true;
+            } else {
+                
+            }
+
+        }
+        return mybool;
     }
 
 }
